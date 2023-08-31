b2fb6fd5a7650811ac5ff4ab8e4edc7c1aa283b9
Olivier Zalmanski <olivier.zalmanski@me.com>
2023-08-31 13:43:54 +0200

    fix eslint

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
 import 'source-map-support/register'
 import { App } from 'homey' // eslint-disable-line import/no-extraneous-dependencies
 import { HomeyAPIV3Local } from 'homey-api'
--- eslint.config.js
+++ eslint.config.js
@@ -68,8 +68,7 @@ const tsCustomRules = {
     },
   ],
   '@typescript-eslint/no-unsafe-assignment': 'off',
-  '@typescript-eslint/no-unsafe-call': 'off',
-  '@typescript-eslint/no-unsafe-member-access': 'off',
+  // '@typescript-eslint/no-unsafe-call': 'off',
   '@typescript-eslint/no-unused-vars': [
     'error',
     {
