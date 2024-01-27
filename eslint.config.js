a7511ba91b019dc1ca6d8a2f5be0b0bd647aafc2
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-01-27 02:08:39 +0100

    improve

--- eslint.config.js
+++ eslint.config.js
@@ -5,55 +5,51 @@ const tsPlugin = require('@typescript-eslint/eslint-plugin')
 const importPlugin = require('eslint-plugin-import')
 const globals = require('globals')
 
-const customRules = {
-  'max-lines': 'off',
-  'no-ternary': 'off',
-  'no-underscore-dangle': ['error', { allow: ['__'] }],
-  'one-var': 'off',
-}
-const tsCustomRules = {
-  // eslint-disable-next-line no-magic-numbers
-  '@typescript-eslint/no-magic-numbers': ['error', { ignore: [0] }],
-  '@typescript-eslint/no-unused-vars': [
-    'error',
-    { varsIgnorePattern: 'onHomeyReady' },
-  ],
-  '@typescript-eslint/prefer-readonly-parameter-types': 'off',
-  'import/extensions': 'off',
-  'import/no-duplicates': ['error', { 'prefer-inline': true }],
-}
-
 module.exports = [
   { ignores: ['.homeybuild/'] },
   {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    linterOptions: { reportUnusedDisableDirectives: true },
+    plugins: { import: importPlugin },
     rules: {
       ...js.configs.all.rules,
       ...importPlugin.configs.recommended.rules,
-      ...customRules,
+      'max-lines': 'off',
+      'no-ternary': 'off',
+      'no-underscore-dangle': ['error', { allow: ['__'] }],
+      'one-var': 'off',
     },
+    settings: { 'import/resolver': { typescript: { alwaysTryTypes: true } } },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: { globals: globals.node },
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: './tsconfig.json' },
+    },
     plugins: { '@typescript-eslint': tsPlugin },
     rules: {
       // eslint-disable-next-line no-magic-numbers
       ...tsPlugin.configs['eslint-recommended'].overrides[0].rules,
       ...tsPlugin.configs.all.rules,
-      ...tsCustomRules,
+      // eslint-disable-next-line no-magic-numbers
+      '@typescript-eslint/no-magic-numbers': ['error', { ignore: [0] }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { varsIgnorePattern: 'onHomeyReady' },
+      ],
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+      'import/extensions': 'off',
+      'import/no-duplicates': ['error', { 'prefer-inline': true }],
     },
   },
   importPlugin.configs.typescript,
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      globals: globals.node,
-      parser: tsParser,
-      parserOptions: { project: './tsconfig.json' },
-      sourceType: 'module',
-    },
-    linterOptions: { reportUnusedDisableDirectives: true },
-    plugins: { import: importPlugin },
-    settings: { 'import/resolver': { typescript: { alwaysTryTypes: true } } },
-  },
   prettier,
 ]
