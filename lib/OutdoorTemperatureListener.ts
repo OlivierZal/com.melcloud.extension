e681462e4495e7e534a2a658707060d1b222873a
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-03-21 11:02:58 +0100

    Update OutdoorTemperatureListener.ts
    Signed-off-by: Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>

--- lib/OutdoorTemperatureListener.ts
+++ lib/OutdoorTemperatureListener.ts
@@ -1,5 +1,5 @@
 /* eslint-disable
-@typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
+  @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
 */
 import type {
   CapabilityValue,
