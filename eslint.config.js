13da4172c5ac93c9cc30bfc4495107c8c8fd5c35
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-03-30 00:53:07 +0100

    improve

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ module.exports = [
     rules: {
       ...js.configs.all.rules,
       ...importPlugin.configs.recommended.rules,
+      '@stylistic/lines-between-class-members': 'error',
       'max-lines': 'off',
       'no-ternary': 'off',
       'no-underscore-dangle': ['error', { allow: ['__'] }],
@@ -176,6 +177,5 @@ module.exports = [
       },
     },
   },
-  { rules: { '@stylistic/lines-between-class-members': 'error' } },
   prettier,
 ]
