f57c5062d66f2da5c3ed6ded5f737780e9d64fd6
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-05-22 01:09:14 +0200

    improve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -260,7 +260,7 @@ export default tsEslint.config(
     },
   },
   {
-    extends: [tsEslint.configs.disableTypeChecked],
     files: ['**/*.mjs'],
+    ...tsEslint.configs.disableTypeChecked,
   },
 )
